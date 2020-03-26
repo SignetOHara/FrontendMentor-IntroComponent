a6e716cab611f2e5bbead1f352040833ec984c37
Alex Friedman <alexbfriedman@googlemail.com>
2020-03-26 20:37:19 +0800

    finised JS

--- script.js
+++ script.js
@@ -1,36 +1,69 @@
-// const form = document.querySelector(".form");
-// const formInputSection = document.querySelector(".form__input--section")
-// const button = document.querySelector('.button');
+const form = document.getElementById('form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
-// const errorFirstName = document.getElementById("form--errormsg__firstName")
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	validateInputs();
+});
 
-// form.addEventListener('submit', (e) => {
-  
-//   if (firstName.value.trim() === '' || firstName.value == null) {
-//     e.preventDefault()
-//     firstName.appendChild
-//     errorFirstName.innerHTML = 'First Name cannot be empty';
-//   }
-  
-// })
+function validateInputs() {
+	const firstNameValue = firstName.value.trim();
+	const lastNameValue = lastName.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const button = document.querySelector('.button');
+	const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-// function validateEmail(email) {
-//   let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   return valid.test(String(email).toLowerCase());
-// }
-
-const emailInput = document.getElementById('email');
-const button = document.querySelector('.button');
-const regEx = /\S+@\S+\.\S+/;
-
-function validate(e) {
-  e.preventDefault();
-	if (regEx.test(emailInput.value)) {
-		console.log('Email valid');
+	if (firstNameValue === '') {
+		setErrorFor(firstName, 'First Name cannot be empty');
 	} else {
-    emailInput.style.background = "url(images/icon-error.svg) no-repeat 90%"
-		emailInput.style.border = "medium solid hsl(0, 100%, 74%)";
+		setSuccessFor(firstName);
+	}
+
+	if (lastNameValue === '') {
+		setErrorFor(lastName, 'Last Name cannot be empty');
+	} else {
+		setSuccessFor(lastName);
+	}
+
+	if (regEx.test(emailValue)) {
+		setSuccessFor(email);
+	} else {
+		setErrorFor(email, 'Looks like this is not an email');
+	}
+
+	if (passwordValue === '') {
+		setErrorFor(password, 'Password cannot be empty');
+	} else {
+		setSuccessFor(password);
+	}
+
+	if (firstName.classList.contains('success') && lastName.classList.contains('success') && email.classList.contains('success') && password.classList.contains('success')) {
+		button.innerText = "You are registered!";
+		button.disabled = 'true';
+		button.style.background = 'hsl(154, 77%, 63%)';
+		button.style.cursor = 'inherit';
 	}
 }
 
-button.addEventListener('click', validate);
+function setErrorFor(input, message) {
+	let targetInput = input;
+	const errorMsg = targetInput.nextElementSibling;
+
+	input.placeholder = '';
+	errorMsg.innerText = message;
+	targetInput = input.classList.add('invalid');
+	errorMsg.style.display = 'block';
+}
+
+function setSuccessFor(input) {
+	let targetInput = input;
+	const errorMsg = targetInput.nextElementSibling;
+
+	targetInput = input.classList.add('success');
+	targetInput = input.classList.remove('invalid');
+	errorMsg.style.display = 'none';
+}
